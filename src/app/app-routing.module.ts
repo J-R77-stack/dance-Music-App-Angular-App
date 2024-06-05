fc1a7cbd81f08980cb758cef770b5dc5d1e76be3
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-06-05 15:00:52 +0100

    create music-list and home component ans set up routing between them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MusicListComponent } from './pages/music-list/music-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'music-list', component: MusicListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
