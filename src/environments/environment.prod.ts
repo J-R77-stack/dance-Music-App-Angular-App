ef21e4c0a16ab46a4f3a8591d8db0cd33af9bf54
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-07-11 18:52:19 +0100

    edit environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   spotifyClientId: '7a31dab292244ff9ac0a830ba5142c6f',
-  spotifyRedirectUri: 'https://top-tracks-app.vercel.app/callback',
+  spotifyRedirectUri: 'https://dance-music-app-angular-app.vercel.app/callback',
 };
