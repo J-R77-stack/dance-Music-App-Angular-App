f11cb5fe871baefcdbcbb33e0b03c9c066b92a82
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-07-11 18:41:58 +0100

    edit environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   spotifyClientId: '7a31dab292244ff9ac0a830ba5142c6f',
-  spotifyRedirectUri: 'https://j-r77-stack.github.io/top-tracks-app/callback',
+  spotifyRedirectUri: 'https://top-tracks-app.vercel.app/callback',
 };
